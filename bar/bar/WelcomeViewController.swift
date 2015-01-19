7ebdfa50a22e6b9fa4d9fec682f82ae23444c4e8
Ben Boral <bboral@rmn.com>
2015-01-18 18:55:42 -0600

    Added Welcome View

--- bar/bar/WelcomeViewController.swift
+++ bar/bar/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  bar
 //
 //  Created by Ben Boral on 1/18/15.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WelcomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
